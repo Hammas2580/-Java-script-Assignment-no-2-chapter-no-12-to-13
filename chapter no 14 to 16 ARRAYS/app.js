f51f7cd7a35cf3c13c49601fd9b299081c0a85bf
Hammas2580 <hasirali653@gmail.com>
2024-10-01 12:47:17 +0500

    array chapter 8 question complete

---
+++ chapter no 14 to 16 ARRAYS/app.js
@@ -0,0 +1,63 @@
+// ******************* chapter no 14 to 16 Arrays  ****************************
+
+// ==================== Question no 1 ===============================
+
+// var studentName = [];
+
+// ==================== Question no 3 ===============================
+  
+var strArr = ["array"];
+console.log("string array" , strArr);
+
+
+// ==================== Question no 4 ===============================
+
+var numberArr = [100];
+console.log("number array" , numberArr);
+
+
+// ==================== Question no 5 ===============================
+
+var boolArr = false;
+console.log("boolean array" , boolArr);
+
+// ==================== Question no 6 ===============================
+
+
+var mixedArr = ["hammas" , "hasham" , 100 , 7 , true];
+console.log(mixedArr);
+
+
+// ==================== Question no 7 ===============================
+
+
+var educationQualification = ["SSC" , "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.PHIL" , "PHD"];
+console.log(educationQualification[0]);
+console.log(educationQualification[1]);
+console.log(educationQualification[2]);
+console.log(educationQualification[3]);
+console.log(educationQualification[4]);
+console.log(educationQualification[5]);
+console.log(educationQualification[6]);
+console.log(educationQualification[7]);
+console.log(educationQualification.length);
+
+// ==================== Question no 8 ===============================
+
+
+var studentName = ["micheal" , "john" , "tony"];
+var score = [478, 298 , 643];
+var totalMarks = [500];
+
+var percenatge = (478 / 500) * 100;
+var percenatge1 = (298 / 500) * 100;
+var percenatge2 = (643 / 500) * 100;
+
+console.log(" Score of micheal is" + " " + score[0] + " Percentage" + " " + percenatge);
+console.log(" Score of john is" + " " + score[1] + " Percentage" + " " + percenatge1);
+console.log(" Score of tony is" + " " + score[2] + " Percentage" + " " + percenatge2);
+
+
+
+
+
